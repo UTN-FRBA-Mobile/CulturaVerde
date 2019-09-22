5bbf240a95bbb0dfeaadf1c23353eab645dfb6f6
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-09-22 13:22:17 -0300

    agregue nav_graph

--- app/src/main/java/com/example/culturaverde/MainActivity.java
+++ app/src/main/java/com/example/culturaverde/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.culturaverde;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.activity_main);
     }
 }
