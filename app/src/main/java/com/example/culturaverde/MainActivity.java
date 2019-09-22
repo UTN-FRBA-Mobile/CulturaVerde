ecc2b64d418641a340751bf1bf5de21111f8e613
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-09-22 14:53:59 -0300

    Inicia con pantalla login

--- app/src/main/java/com/example/culturaverde/MainActivity.java
+++ app/src/main/java/com/example/culturaverde/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.culturaverde;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.navigation.fragment.*;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_login_main);
+
     }
-}
+ }
