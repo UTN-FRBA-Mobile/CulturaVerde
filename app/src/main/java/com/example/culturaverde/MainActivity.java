64ca27f0aed042e4912080b965bd0850d529eb89
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-09-24 20:41:59 -0300

    conectar pantallas

--- app/src/main/java/com/example/culturaverde/MainActivity.java
+++ app/src/main/java/com/example/culturaverde/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.culturaverde;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.os.Bundle;
-import androidx.navigation.fragment.*;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login_main);
+        setContentView(R.layout.activity_main);
+        //Declaramos un objeto tipo Toolbar y lo instanciamos
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //val host = NavHostFragment.create(R.navigation.nav_graph);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,NavHostFragment.create(R.navigation.nav_graph)).setPrimaryNavigationFragment(NavHostFragment.create(R.navigation.nav_graph)).commit();
 
     }
  }
