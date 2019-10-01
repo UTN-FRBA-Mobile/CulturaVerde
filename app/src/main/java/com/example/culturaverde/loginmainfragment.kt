db316cbcb92e65bf97b99f593bbd51689be8d6f8
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-10-01 20:12:31 -0300

    nueva version con kotlin

---
+++ app/src/main/java/com/example/culturaverde/loginmainfragment.kt
@@ -0,0 +1,33 @@
+package com.example.culturaverde
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_loginmain.*
+
+class loginmainfragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_loginmain, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        botonPlayConsumidores.setOnClickListener{
+
+            val action = loginmainfragmentDirections.actionLoginmainfragmentToPrincipalconsumidoresFragment()
+            findNavController().navigate(action)
+
+        }
+
+    }
+
+}
