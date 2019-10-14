c3a2eed735f970f6ed576980b7dcc35a9e31004a
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-10-14 08:52:43 -0300

    .

--- app/src/main/java/com/example/culturaverde/Activities/MapsActivity.kt
+++ app/src/main/java/com/example/culturaverde/Activities/MapsActivity.kt
@@ -1,19 +1,19 @@
 package com.example.culturaverde.Activities
 
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.culturaverde.R
 import com.google.android.gms.location.FusedLocationProviderClient
-
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
@@ -24,7 +24,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onMarkerClick(p0: Marker?)= false
 
     private lateinit var map: GoogleMap
-    private lateinit var locationclient: FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,33 +35,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        //latitudOrigen = location.latitude
-        //longitudOrigen = location.longitude
-
-        //val position = LatLng(latitudOrigen, longitudOrigen)
-
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        map.getUiSettings().setZoomControlsEnabled(true)
         map.setOnMarkerClickListener(this)
-        map.uiSettings.isZoomControlsEnabled = true
 
         setUpMap()
 
     }
 
-    //pide permiso para acceder a la ubicacion
 
-    private fun setUpMap () {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        { ActivityCompat.requestPermissions (this, arrayOf (android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+
+    private fun setUpMap() {
+
+        //pide permiso para acceder a la ubicacion
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
             return
+        }
+
+
+        //encuentra nuestra ubicacion
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
         }
     }
 }
