6ddb9d90f90a03e51b85abaad5773fe45534cbf2
ezebosso <eze.bosso@gmail.com>
2019-10-14 11:58:32 -0300

    controlador

---
+++ app/src/main/java/com/example/culturaverde/Controllers/UsuarioControlador.kt
@@ -0,0 +1,21 @@
+package com.example.culturaverde.Controllers
+
+import io.reactivex.Observable
+import retrofit2.Response
+import retrofit2.http.*
+import com.example.culturaverde.Models.Usuario.User
+
+interface UsuarioControlador {
+
+        @POST("users")
+        fun create(@Body user: User): Observable<User>
+
+        @GET("users/{username}")
+        fun read(@Path("username") username: String): Observable<User>
+
+        @PUT("users/{username}")
+        fun update(@Path("username") username: String, @Body user: User): Observable<User>
+
+        @DELETE("users/{username}")
+        fun delete(@Path("username") username: String): Observable<Response<Void>>
+    }
