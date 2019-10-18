dcde678ff4785fe14eda5987477a457dcc4b5078
ezebosso <eze.bosso@gmail.com>
2019-10-18 17:12:14 -0300

    usuario controlador

--- app/src/main/java/com/example/culturaverde/Models/Usuario.kt
+++ app/src/main/java/com/example/culturaverde/Models/Usuario.kt
@@ -3,7 +3,12 @@ package com.example.culturaverde.Models
 import java.util.*
 
 object Usuario {
-    data class User(val id: Int, val nombre: String, val apellido: String, val usuario: String,
-                    val contraseña: String, val fecha_nacimiento: Date, val rol: String, val telefono: String)
-
-}
+    data class User(val id: Int,
+                    val nombre: String,
+                    val apellido: String,
+                    val usuario: String,
+                    val contraseña: String,
+                    val fecha_nacimiento: Date,
+                    val rol: String,
+                    val telefono: String)
+                }
