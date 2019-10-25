ec7378876d15ca0c70d333dddac0da8a4f67c7f6
ezebosso <eze.bosso@gmail.com>
2019-10-25 16:08:47 -0300

    Update APIConfig.kt

--- app/src/main/java/com/example/culturaverde/Services/APIConfig.kt
+++ app/src/main/java/com/example/culturaverde/Services/APIConfig.kt
@@ -10,7 +10,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 object APIConfig {
 
 
-    val BASE_URL = "http://192.168.7.123:8080/redAgro/"
+    val BASE_URL = "http://192.168.0.107:8080/redAgro/"
 
 
     private var retrofit: Retrofit? = null
