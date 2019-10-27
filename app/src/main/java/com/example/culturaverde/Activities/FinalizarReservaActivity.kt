62b442a9c3ad02714a3163e3a15ce94616aba900
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-10-27 16:39:57 -0300

    direccionar a pantalla finalizar reserva

---
+++ app/src/main/java/com/example/culturaverde/Activities/FinalizarReservaActivity.kt
@@ -0,0 +1,17 @@
+package com.example.culturaverde.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.culturaverde.R
+
+
+class FinalizarReservaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_finalizarreserva)
+
+
+    }
+
+}
