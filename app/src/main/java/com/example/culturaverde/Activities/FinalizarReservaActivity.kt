2bcc6d26a21de76317ed8159e9c129b9de6c0ce7
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-10-28 00:23:28 -0300

    Pantalla Elegir punto de entrega

--- app/src/main/java/com/example/culturaverde/Activities/FinalizarReservaActivity.kt
+++ app/src/main/java/com/example/culturaverde/Activities/FinalizarReservaActivity.kt
@@ -1,8 +1,13 @@
 package com.example.culturaverde.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.culturaverde.R
+import kotlinx.android.synthetic.main.fragment_finalizarreserva.*
+import android.widget.CheckBox
+import kotlinx.android.synthetic.main.activity_shopping_cart.*
 
 
 class FinalizarReservaActivity : AppCompatActivity() {
@@ -11,6 +16,24 @@ class FinalizarReservaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_finalizarreserva)
 
+
+        var checkBox = findViewById<CheckBox>(R.id.checkBoxRetiraOtraPersona)
+        checkBox?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (checkBoxRetiraOtraPersona.isChecked) {
+                usuarioreserva.setText("corbocecilia@gmail.com")
+                contactoreserva.setText("123456789")
+            } else {
+                usuarioreserva.setText("corbocecilia@gmail.com")
+                nombrereserva.setText("Cecilia")
+                apellidoreserva.setText("Corbo")
+                contactoreserva.setText("123456789")
+            }
+        }
+
+        botonContinuarReserva.setOnClickListener {
+
+            startActivity(Intent(this, ElegirPuntoEntregaRetiroActivity::class.java))
+        }
 
     }
 
