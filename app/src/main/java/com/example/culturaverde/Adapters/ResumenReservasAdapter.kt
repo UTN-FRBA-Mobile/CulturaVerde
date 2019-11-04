8c28eb755fa5104fbfe22bb68834fe9e5f15a17b
corbocecilia <50270544+corbocecilia@users.noreply.github.com>
2019-11-04 01:10:31 -0300

    reduccion tamaño

--- app/src/main/java/com/example/culturaverde/Adapters/ResumenReservasAdapter.kt
+++ app/src/main/java/com/example/culturaverde/Adapters/ResumenReservasAdapter.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.culturaverde.Classes.ShoppingCart
 import com.example.culturaverde.Models.CartItem
 import com.example.culturaverde.R
 import kotlinx.android.synthetic.main.cart_list_item.view.product_name
 import kotlinx.android.synthetic.main.cart_list_item.view.product_quantity
+import kotlinx.android.synthetic.main.fragment_resumenreservas.view.*
 import kotlinx.android.synthetic.main.resumenfinal_list_item.view.*
 
 class ResumenReservasAdapter(var context: Context, var cartItems: List<CartItem>) :
@@ -46,15 +48,21 @@ class ResumenReservasAdapter(var context: Context, var cartItems: List<CartItem>
             val imageBytes = Base64.decode(cartItem.product.imagenes[0].image, 0)
             val image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
 
+            var totalPrice = cartItem.quantity.times(cartItem.product.precio!!.toDouble())
+
             itemView.product_image3.setImageBitmap(image)
 
             itemView.product_name.text = cartItem.product.titulo
 
-            itemView.product_price.text = "$${cartItem.product.precio}"
-
             itemView.product_quantity.text = cartItem.quantity.toString()
 
             itemView.product_tipounidad.text = cartItem.product.unidad_venta
+
+            itemView.product_price.text = "$${cartItem.product.precio}"
+
+            itemView.product_price_text.text = "por c/u"
+
+            //itemView.totalreserva.text = totalPrice.toString()
 
         }
     }
