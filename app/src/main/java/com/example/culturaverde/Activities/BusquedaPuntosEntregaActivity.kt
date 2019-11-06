09a79d9efb5e5aa0a5bcfb68f36ffe8005007de1
Federico Fernandez <fernandezfede91@gmail.com>
2019-11-06 00:06:22 -0300

    Avances en generacion de reserva

--- app/src/main/java/com/example/culturaverde/Activities/BusquedaPuntosEntregaActivity.kt
+++ app/src/main/java/com/example/culturaverde/Activities/BusquedaPuntosEntregaActivity.kt
@@ -1,17 +1,78 @@
 package com.example.culturaverde.Activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.culturaverde.Adapters.PuntosEntregaAdapter
+import com.example.culturaverde.Controllers.PuntosentregaControlador
+import com.example.culturaverde.Models.PuntosEntrega
 import com.example.culturaverde.R
+import com.example.culturaverde.Services.APIConfig
+import com.example.culturaverde.ViewModels.BusquedaPuntosEntregaViewModel
+import kotlinx.android.synthetic.main.fragment_busquedapuntosentrega.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BusquedaPuntosEntregaActivity: AppCompatActivity() {
+
+    private var puntosEntregaControlador: PuntosentregaControlador = APIConfig.getRetrofitClient(this).create(PuntosentregaControlador::class.java)
+
+    private lateinit var puntosEntregaAdapter: PuntosEntregaAdapter
+    private var puntosentrega = listOf<PuntosEntrega>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_busquedapuntosentrega)
 
+        swipeRefreshLayout4.setColorSchemeColors(ContextCompat.getColor(this, R.color.color_verde))
+
+        swipeRefreshLayout4.isRefreshing = true
+
+       recycler_puntosentrega.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+
+        getpuntosdeentrega()
+
     }
 
+    fun getpuntosdeentrega() {
+        puntosEntregaControlador.obtenerpuntosdeentrega()
+            .enqueue(object : Callback<List<PuntosEntrega>> {
+                override fun onFailure(call: Call<List<PuntosEntrega>>, t: Throwable) {
+
+
+                    Toast.makeText(
+                        this@BusquedaPuntosEntregaActivity,
+                        "Ocurrió un error inesperado, intentá nuevamente",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+                override fun onResponse(
+                    call: Call<List<PuntosEntrega>>,
+                    response: Response<List<PuntosEntrega>>
+                ) {
+
+                    swipeRefreshLayout4.isRefreshing = false
+                    swipeRefreshLayout4.isEnabled = false
+
+                    if (response != null) {
+
+                        puntosentrega = response.body()!!
+
+                        puntosEntregaAdapter = PuntosEntregaAdapter(this@BusquedaPuntosEntregaActivity, puntosentrega)
+                        recycler_puntosentrega.adapter = puntosEntregaAdapter
+                        puntosEntregaAdapter.notifyDataSetChanged()
+                    }
+                }
+
+            })
+    }
 
 }
